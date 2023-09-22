aa3106efea1cda63f904874d282e08e81498adb2
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-09-22 14:50:39 -0300

    a

--- js/main/app.js
+++ js/main/app.js
@@ -1,25 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const routes = require('./js/main/routes'); // Importe o arquivo de rotas
-const ejs = require('ejs'); // Importe o pacote EJS
+const routes = require('./routes'); 
+const ejs = require('ejs'); 
 
 app.use('/novo-kmeans.js', (req, res) => {
   res.type('application/javascript');
   res.sendFile(path.join(__dirname, 'public/js/main/novo-kmeans.js'));
 });
 
-// Configuração do diretório de arquivos estáticos (CSS, JavaScript, imagens, etc.)
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Configuração para usar EJS como mecanismo de visualização
-app.set('view engine', 'ejs'); // Defina o mecanismo de visualização como EJS
-app.set('views', path.join(__dirname, 'js/main/views')); // Defina o diretório de visualização
 
-// Use as rotas definidas no arquivo de rotas
+app.set('view engine', 'ejs'); 
+
+app.set('views', path.join(__dirname, 'js/main/views')); 
+
+
 app.use('/', routes);
 
-// Porta em que o servidor irá escutar
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
--- js/main/routes.js
+++ js/main/routes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// Rota para a página inicial
 router.get('/', (req, res) => {
-  res.render('index'); // Renderiza a página index.ejs
+  
+  res.render('index');
 });
 
-// Rota para processar o upload de arquivos (se necessário)
+
 router.post('/upload', (req, res) => {
-  // Coloque o código para processar o upload aqui, se necessário
-  // Depois, redirecione ou envie uma resposta adequada
+  
 });
 
 module.exports = router;
