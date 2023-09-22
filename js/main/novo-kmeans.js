989b8adff99969244a190cdcd03fd436a82c6582
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-09-22 10:34:22 -0300

    ajustes

--- js/main/novo-kmeans.js
+++ js/main/novo-kmeans.js
@@ -1,5 +1,5 @@
 const kmeans = require('node-kmeans');
- 
+
 // Sample data
 const data = [
     [1, 2],
@@ -9,19 +9,19 @@ const data = [
     [11, 12],
     [12, 13]
 ];
- 
+
 // Number of clusters
-const k = 3;
- 
+const k = 3; // Alterado para 3 clusters
+
 // Perform k-means clustering
 kmeans.clusterize(data, { k }, (err, result) => {
     if (err) {
         console.error(err);
         return;
     }
- 
+
     // Cluster assignments for each data point
     const clusterAssignments = result.map(point => point.cluster);
- 
+
     console.log('Cluster assignments:', clusterAssignments);
 });
