e1225bfb0ca6f57433da9a976e0bc9dc2c149cfb
José Carlos Moura <124727238+zecamoura@users.noreply.github.com>
2023-09-22 11:38:44 -0300

    alterçoes visando rodar o index.ejs ao inves do arquivo .html

--- js/main/novo-kmeans.js
+++ js/main/novo-kmeans.js
@@ -1,5 +1,5 @@
 const kmeans = require('node-kmeans');
-
+ 
 // Sample data
 const data = [
     [1, 2],
@@ -7,21 +7,24 @@ const data = [
     [3, 4],
     [10, 11],
     [11, 12],
-    [12, 13]
+    [12, 13],
+    [13,14],
+    [14,15],
+    [15,16]
 ];
-
+ 
 // Number of clusters
-const k = 3; // Alterado para 3 clusters
-
+const k = 3;
+ 
 // Perform k-means clustering
 kmeans.clusterize(data, { k }, (err, result) => {
     if (err) {
         console.error(err);
         return;
     }
-
+ 
     // Cluster assignments for each data point
     const clusterAssignments = result.map(point => point.cluster);
-
+ 
     console.log('Cluster assignments:', clusterAssignments);
 });
