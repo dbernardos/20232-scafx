71e01c84e53efec4dede76399287a90200bad63e
dbernardos <121075844+dbernardos@users.noreply.github.com>
2023-09-22 00:55:30 -0300

    novo kmeans
    esse está agrupando corretamente

---
+++ js/main/novo-kmeans.js
@@ -0,0 +1,27 @@
+const kmeans = require('node-kmeans');
+ 
+// Sample data
+const data = [
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [10, 11],
+    [11, 12],
+    [12, 13]
+];
+ 
+// Number of clusters
+const k = 3;
+ 
+// Perform k-means clustering
+kmeans.clusterize(data, { k }, (err, result) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+ 
+    // Cluster assignments for each data point
+    const clusterAssignments = result.map(point => point.cluster);
+ 
+    console.log('Cluster assignments:', clusterAssignments);
+});
